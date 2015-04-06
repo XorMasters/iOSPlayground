c791122a1c0274a1ab2dd268427f3388763aa30f
Allan Knight <harveytee@gmail.com>
2015-04-06 00:02:41 -0700

    initial calculator code

---
+++ Calculator/Calculator/Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Allan Knight on 4/6/15.
+//  Copyright (c) 2015 Allan Knight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculator/Calculator/Calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Allan Knight on 4/6/15.
+//  Copyright (c) 2015 Allan Knight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
